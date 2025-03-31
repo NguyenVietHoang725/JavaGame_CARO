2bd14af421a74827e883b50e3cc87ff355a0a7a0
Harry <hoangnv725@gmail.com>
2025-03-31 21:13:56 +0700

    update with InputMismatchException

--- src/com/caro/utils/InputHandler.java
+++ src/com/caro/utils/InputHandler.java
@@ -1,16 +1,24 @@
 package com.caro.utils;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputHandler {
 	private Scanner scn = new Scanner(System.in);
 
-	public int[] getInput() {
-		int row = scn.nextInt();
-		int col = scn.nextInt();
-		
-		return new int[] {row - 1, col - 1};
-		
+	public int[] getInput() throws InputMismatchException {
+		try {
+			int row = scn.nextInt();
+			int col = scn.nextInt();
+
+			return new int[] { row - 1, col - 1 };
+		} catch (InputMismatchException e) {
+			throw new InputMismatchException("Invalid input. Please enter integers only.");
+		}
+	}
+
+	public void clear() {
+		scn.nextLine(); // Clear the invalid input from the buffer
 	}
 
 	public void close() {
